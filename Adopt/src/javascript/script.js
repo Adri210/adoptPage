badcd024e38ae4e16e37923056b7433f264428b5
Adriani Delfino <141968057+Adri210@users.noreply.github.com>
2024-05-31 19:46:49 -0300

    finish

--- Adopt/src/javascript/script.js
+++ Adopt/src/javascript/script.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
         distance: '20%'
     });
 
-    ScrollReveal().reveal('#testimonial_chef', {
+    ScrollReveal().reveal('#testimonial_person', {
         origin: 'left',
         duration: 1000,
         distance: '20%'
